f40be863a86fd14186bc2c4bfb037915092958c0
Marcel Stadelmann <m@marceli.to>
2022-10-24 13:42:19 +0200

    update to laravel 7

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,8 +1,6 @@
 <?php
-
 namespace App\Exceptions;
-
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -29,10 +27,10 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -41,10 +39,10 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         if ($this->isHttpException($exception))
         {
